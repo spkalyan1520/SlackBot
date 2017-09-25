e139cfaa8e6600d5b8214104061901dc8202f77b
ksripath <ksripath@tibco.com>
2017-09-25 16:21:53 -0700

    Refactored classes

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 var settings = require('./config/slack-settings.config');
 var BandwidthBot = require('./lib/bandwidthbot');
 
-var bot= new BandwidthBot(settings);
+var bot= new BandwidthBot(settings,function(){
+ console.log('Started');
+}, function(msg){
+    console.log(msg);
+});
 bot.run();
