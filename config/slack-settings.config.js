8b543c264842bf4e904f48e668e0c5dadbe00731
ksripath <ksripath@tibco.com>
2017-09-25 16:16:17 -0700

    Refactored classes

---
+++ config/slack-settings.config.js
@@ -0,0 +1,2 @@
+module.exports.token = 'xoxb-246125849703-pZ23FaMQistOdXJkXw00yPLw';
+module.exports.name ='botuser';
--- test.js
+++ test.js
@@ -1,18 +1,5 @@
-var Bot = require('slackbots');
+var settings = require('./config/slack-settings.config');
+var BandwidthBot = require('./lib/bandwidthbot');
 
-// create a bot
-var settings = {
-    token: 'xoxb-246125849703-B575yXq94d5bUh8r3efILjYX',
-    name: 'botuser'
-};
-var bot = new Bot(settings);
-
-bot.on('start', function() {
-   // bot.postMessageToChannel('general', 'Hello channel!');
-   // bot.postMessageToUser('kalyansripathi', 'hello bro!');
-    //bot.postMessageToGroup('general', 'hello group chat!');
-});
-
-bot.on('message',function(msg){
-    console.log(msg);
-});
+var bot= new BandwidthBot(settings);
+bot.run();
