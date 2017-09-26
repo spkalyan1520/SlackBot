66bf96bcf57e9ff82d4fcd6b39b3d37268f105cc
Kalyan Sripathi <kalyansripathi@gmail.com>
2017-09-26 00:33:24 -0500

    Refactored classes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var settings = require('./config/slack-settings.config');
 var BandwidthBot = require('./lib/bandwidthbot');
 
-var bot= new BandwidthBot(settings,function(){
- console.log('Started');
-}, function(msg){
-    console.log(msg);
-});
+var bot= new BandwidthBot(settings);
 bot.run();
+
+//On start
+bot.on('start',function(){
+   console.log('Started');
+});
+
+//On Message
+bot.on('message',function(msg){
+   console.log(msg);
+});
